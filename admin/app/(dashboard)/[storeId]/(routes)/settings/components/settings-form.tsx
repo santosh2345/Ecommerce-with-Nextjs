72eea7f2af00c09399368fbb656ed99a255e3415
santosh2345 <santoshgiri2345@gmail.com>
2024-01-10 10:10:18 +0545

    setting form schema

--- admin/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ admin/app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,37 +1,40 @@
-"use client"
+"use client";
 
 import { Button } from "@/components/ui/button";
 import Heading from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
-import { Store } from "@prisma/client"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Store } from "@prisma/client";
 import { Trash } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
 interface SettingsFormProps {
-    initialData: Store;
-    }
-
-const SettingsForm:React.FC<SettingsFormProps> = ({
-    initialData
-}) => {
-  return (
-    <>
-    <div className="flex items-center justify-between">
-        <Heading
-        title = "Settings"
-        description = "Update your store settings"
-        />
-        <Button
-        variant='destructive'
-        size='icon'
-        onClick={()=>{}}
-        >
-            <Trash className="h-4 w-4" />
-
-        </Button>
-        </div>
-        <Separator />   
-        </>
-  )
+  initialData: Store;
 }
 
-export default SettingsForm
+const formSchema = z.object({
+  name: z.string().min(1),
+});
+
+type SettingsFormValues = z.infer<typeof formSchema>;
+
+const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
+  const form = useForm<SettingsFormValues>({
+    defaultValues: initialData,
+    resolver: zodResolver(formSchema),
+  });
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading title="Settings" description="Update your store settings" />
+        <Button variant="destructive" size="icon" onClick={() => {}}>
+          <Trash className="h-4 w-4" />
+        </Button>
+      </div>
+      <Separator />
+    </>
+  );
+};
+
+export default SettingsForm;

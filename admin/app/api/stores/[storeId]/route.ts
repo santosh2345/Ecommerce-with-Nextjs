f0cfec8ac1c08fd52ce40e4f59d6fdf1f3018b62
santosh2345 <santoshgiri2345@gmail.com>
2024-01-10 11:34:39 +0545

    route for update store name

--- admin/app/api/stores/[storeId]/route.ts
+++ admin/app/api/stores/[storeId]/route.ts
@@ -1,3 +1,5 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
 export async function PATCH(
@@ -5,6 +7,33 @@ export async function PATCH(
     {params}:{params:{storeId: string}},
 ){
     try {
+        const { userId } = auth();
+
+        const body = await req.json();
+        const { name } = body;
+
+
+        if(!userId){
+            return new NextResponse("Unauthenticated", { status: 401 });
+        }
+        if(!name){
+            return new NextResponse("Name is required", { status: 400 });
+        }
+        if(!params.storeId){
+            return new NextResponse("Store ID is required", { status: 400 });
+        }
+
+        const store = await prismadb.store.updateMany({
+            where:{
+                id: params.storeId,
+                userId,
+            },
+            data:{
+                name,
+            },
+        })
+
+        return NextResponse.json(store);
         
     } catch (error) {
         console.log("[STORE_PATCH]", error);

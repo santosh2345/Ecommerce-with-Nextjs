471c9945c85a6d36692d788a7ea0cf43b468bbb5
santosh2345 <santoshgiri2345@gmail.com>
2024-01-10 09:52:04 +0545

    routing

--- admin/app/api/stores/route.ts
+++ admin/app/api/stores/route.ts
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 export async function POST(req: Request) {
   try {
     const { userId } = auth();
-    const body = await req.json();
+    const body = await req.json();a
     const { name } = body;
 
     if (!userId) {

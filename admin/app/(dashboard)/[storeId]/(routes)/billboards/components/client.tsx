07aba204c8ff068efefb251c00cd0e1909888ad4
santosh2345 <santoshgiri2345@gmail.com>
2024-01-11 11:52:05 +0545

    billboard page

--- admin/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ admin/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -1,11 +1,19 @@
 "use client";
 
+import { useParams, useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import Heading from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import { Plus } from "lucide-react";
 
 export const BillboardClient = () => {
+
+    const router = useRouter()
+    const params = useParams()
+
+
+
+
   return (
     <>
       <div className="flex items-center justify-between">
@@ -14,7 +22,7 @@ export const BillboardClient = () => {
           description="Mange billboards for your store"
         />
 
-        <Button>
+        <Button onClick={()=> router.push(`/${params.storeId}/billboards/new`)}>
           <Plus className="mr-2 h-4 w-4" />
           Add new
         </Button>

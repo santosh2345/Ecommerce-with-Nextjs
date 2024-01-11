076ca579cd25840d9a9b727a7aff6725b56d8d7f
santosh2345 <santoshgiri2345@gmail.com>
2024-01-11 10:25:09 +0545

    window origin return

--- admin/hooks/use-origin.tsx
+++ admin/hooks/use-origin.tsx
@@ -1,11 +1,20 @@
-import { useState } from "react"
+import { useEffect, useState } from "react";
 
+export const useOrigin = () => {
+  const [mounted, setMounted] = useState(false);
 
-export const useOrigin =()=>{
+  const origin =
+    typeof window !== "undefined" && window.location.origin
+      ? window.location.origin
+      : "";
 
-    const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
-    const origin =  typeof window !== "undefined" && window.location.origin? window.location.origin : "";
+  if (!mounted) {
+    return null;
+  }
 
-
-}
+  return origin;
+};

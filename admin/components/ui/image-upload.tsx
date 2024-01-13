b7060c910f2ed7b74257416c84270ae9fe036c89
santosh2345 <santoshgiri2345@gmail.com>
2024-01-13 19:02:17 +0545

    image upload trash

--- admin/components/ui/image-upload.tsx
+++ admin/components/ui/image-upload.tsx
@@ -2,7 +2,10 @@
 
 "use client"
 
+import Image from "next/image";
 import { useEffect, useState } from "react";
+import { Trash } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 interface ImageUploadProps {
     disabled?: boolean;
@@ -38,7 +41,19 @@ const ImageUpload:React.FC<ImageUploadProps> = ({
         <div className="mb-4 flex items-center gap-4">
             {value.map((url)=>(
                 <div key={url} className="relative w-[200px] h-[200px]  rounded-md overflow-hidden">
-                    
+                    <div className="z-10  absolute top-2 right-2">
+                        <Button type="button" onClick={()=>onRemove(url)} variant="destructive" size='icon'>
+
+                            <Trash className="h-4 w-4" />
+
+
+                        </Button>
+                        </div>
+                        <Image 
+                        fill
+                        src={url}
+                        className="object-cover"
+                        />
 
                     </div>
 

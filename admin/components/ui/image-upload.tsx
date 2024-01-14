3c65e7dc46bd60f2cbd73affc7d6dbee5a974d60
santosh2345 <santoshgiri2345@gmail.com>
2024-01-14 21:37:23 +0545

    image upload cloudinary

--- admin/components/ui/image-upload.tsx
+++ admin/components/ui/image-upload.tsx
@@ -69,7 +69,7 @@ const ImageUpload:React.FC<ImageUploadProps> = ({
             </div>
             <div className="mb-4">
 
-                <p className="mb-2">Upload Image soon</p>
+                <p className="mb-2">Upload Image soon comming</p>
                 
                
                

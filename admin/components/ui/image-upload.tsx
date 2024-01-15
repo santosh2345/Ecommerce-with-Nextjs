d5d62e36ab33f3337e55a805e876105bcae1e6b1
santosh2345 <santoshgiri2345@gmail.com>
2024-01-15 20:42:59 +0545

    image upload

--- admin/components/ui/image-upload.tsx
+++ admin/components/ui/image-upload.tsx
@@ -69,8 +69,7 @@ const ImageUpload:React.FC<ImageUploadProps> = ({
             </div>
            
             
-            <CldUploadWidget>
-                
+          
                
                     
            

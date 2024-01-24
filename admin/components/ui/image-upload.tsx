712e247e761fb01f076be0823c1629140f0285e2
santosh2345 <santoshgiri2345@gmail.com>
2024-01-24 21:31:18 +0545

    upload image

--- admin/components/ui/image-upload.tsx
+++ admin/components/ui/image-upload.tsx
@@ -1,82 +1,62 @@
+"use client";
 
-
-"use client"
-
-import { CldUploadWidget } from 'next-cloudinary';
+import { CldUploadWidget } from "next-cloudinary";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { Trash } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
 interface ImageUploadProps {
-    disabled?: boolean;
-    onChange: (value: string)=>void;
-    onRemove: (value: string)=>void;
-    value: string[];
+  disabled?: boolean;
+  onChange: (value: string) => void;
+  onRemove: (value: string) => void;
+  value: string[];
 }
 
-const ImageUpload:React.FC<ImageUploadProps> = ({
-    disabled,
-    onChange,
-    onRemove,
-    value,
+const ImageUpload: React.FC<ImageUploadProps> = ({
+  disabled,
+  onChange,
+  onRemove,
+  value,
 }) => {
+  const [isMounted, setIsMounted] = useState(false);
 
-    const [isMounted, setIsMounted] = useState(false);
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
-    useEffect(()=>{
-        setIsMounted(true);
-    },[]);
+  if (!isMounted) {
+    return null;
+  }
 
-    if(!isMounted){
-        return null;
-    }
+  const onUPload = (result: any) => {
+    onChange(result.info.secure_url);
+  };
 
-    const onUPload = (result: any) =>{
-        onChange(result.info.secure_url);
-        
-    }
-
-    return (
-        <>
-        <div className="mb-4 flex items-center gap-4">
-            {value.map((url)=>(
-                <div key={url} className="relative w-[200px] h-[200px]  rounded-md overflow-hidden">
-                    <div className="z-10  absolute top-2 right-2">
-                        <Button type="button" onClick={()=>onRemove(url)} variant="destructive" size='icon'>
-
-                            <Trash className="h-4 w-4" />
-
-
-                        </Button>
-                        </div>
-                        <Image 
-                        fill
-                        src={url}
-                        className="object-cover"
-                        />
-
-                    </div>
-
-            ))}
-
-
-
-                
-           
-           
-
+  return (
+    <div>
+      <div className="mb-4 flex items-center gap-4">
+        {value.map((url) => (
+          <div
+            key={url}
+            className="relative w-[200px] h-[200px]  rounded-md overflow-hidden"
+          >
+            <div className="z-10  absolute top-2 right-2">
+              <Button
+                type="button"
+                onClick={() => onRemove(url)}
+                variant="destructive"
+                size="icon"
+              >
+                <Trash className="h-4 w-4" />
+              </Button>
             </div>
-           
-            
-          
-               
-                    
-           
+            <Image fill src={url} className="object-cover" alt="Image" />
+          </div>
+        ))}
+      </div>
 
-
-
-        
-        </>
-    )
-}
+      
+    </div>
+  );
+};

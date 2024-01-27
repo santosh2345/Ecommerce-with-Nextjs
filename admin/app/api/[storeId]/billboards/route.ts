5f105cd722d23022cd64fd59e252fc0de8a9bcf9
santosh2345 <santoshgiri2345@gmail.com>
2024-01-27 21:15:51 +0545

    billboard GET completed

--- admin/app/api/[storeId]/billboards/route.ts
+++ admin/app/api/[storeId]/billboards/route.ts
@@ -56,39 +56,12 @@ export async function GET(
   { params }: { params: { storeId: string } }
 ) {
   try {
-    const { userId } = auth();
-    const body = await req.json();
-    const { label, imageUrl } = body;
-
-    if (!userId) {
-      return new NextResponse("Unauthenticated", { status: 401 });
-    }
-    if (!label) {
-      return new NextResponse("Label is required", { status: 400 });
-    }
-    if (!imageUrl) {
-      return new NextResponse("Image URL is required", { status: 400 });
-    }
-
     if (!params.storeId) {
       return new NextResponse("Store id is required", { status: 400 });
     }
 
-    const storeByUserId = await prismadb.store.findFirst({
+    const billboard = await prismadb.billboard.findMany({
       where: {
-        id: params.storeId,
-        userId,
-      },
-    });
-
-    if (!storeByUserId) {
-      return new NextResponse("Unauthorized", { status: 403 });
-    }
-
-    const billboard = await prismadb.billboard.create({
-      data: {
-        label,
-        imageUrl,
         storeId: params.storeId,
       },
     });

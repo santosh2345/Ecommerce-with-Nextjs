ec5e5a3e0e769640c606860aab68c1ac74e9584d
santosh2345 <santoshgiri2345@gmail.com>
2024-02-02 22:09:28 +0545

    data table

--- admin/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ admin/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -5,10 +5,11 @@ import { Button } from "@/components/ui/button";
 import Heading from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import { Plus } from "lucide-react";
-import { Billboard } from "@prisma/client";
+import { BillboardColumn, columns } from "./columns";
+import { DataTable } from "@/components/ui/data-table";
 
 interface  BillboardClientProps{
-  data: Billboard[],
+  data: BillboardColumn[],
 }
 
 export const BillboardClient: React.FC<BillboardClientProps> = ({data}) => {
@@ -34,6 +35,7 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({data}) => {
       </div>
 
       <Separator />
+      <DataTable columns={columns} data={data} />
     </>
   );
 };
--- admin/app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ admin/app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -24,7 +24,7 @@ const Billboards = async ({ params }: { params: { storeId: string } }) => {
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardClient data={billboards} />
+        <BillboardClient data={formattedBillboards} />
       </div>
     </div>
   );

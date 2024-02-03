bb013ba956715d5fe09dbaad5dcca20a69536d5c
santosh2345 <santoshgiri2345@gmail.com>
2024-02-03 21:01:45 +0545

    category client

--- admin/app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/page.tsx
+++ admin/app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/page.tsx
@@ -1,5 +1,5 @@
 import prismadb from "@/lib/prismadb";
-import CategoryForm from "./components/billboard-form";
+import CategoryForm from "./components/category-form";
 
 const CategoryPage = async ({
   params,

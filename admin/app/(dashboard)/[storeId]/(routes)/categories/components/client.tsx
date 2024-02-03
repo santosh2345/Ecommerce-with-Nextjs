73e8a2770ced1612c642c3f3407f2d76d916021c
santosh2345 <santoshgiri2345@gmail.com>
2024-02-03 15:11:30 +0545

    category client

--- admin/app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
+++ admin/app/(dashboard)/[storeId]/(routes)/categories/components/client.tsx
@@ -36,11 +36,11 @@ export const CategoryClient: React.FC<CategoryClientProps> = ({data}) => {
       </div>
 
       <Separator />
-      <DataTable columns={columns} data={data} searchKey="label" />
+      <DataTable columns={columns} data={data} searchKey="name" />
 
       <Heading title="API" description="API calls for categories" />
       <Separator />
-      <ApiList entityName="categories" entitiyIdName="billboardId" />
+      <ApiList entityName="categories" entitiyIdName="categoriesId" />
     </>
   );
 };

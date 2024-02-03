47962b231d27981f5297c301caaa82873dfa2d4a
santosh2345 <santoshgiri2345@gmail.com>
2024-02-03 14:54:02 +0545

    category column

--- admin/app/(dashboard)/[storeId]/(routes)/categories/components/columns.tsx
+++ admin/app/(dashboard)/[storeId]/(routes)/categories/components/columns.tsx
@@ -4,16 +4,22 @@ import { ColumnDef } from "@tanstack/react-table"
 import { CellAction } from "./cell-action"
 
 
-export type BillboardColumn = {
+export type CategoryColumn = {
   id: string
-  label: string
+  name: string
+  billboardLabel: string
   createdAt: string
 }
 
-export const columns: ColumnDef<BillboardColumn>[] = [
+export const columns: ColumnDef<CategoryColumn>[] = [
   {
-    accessorKey: "label",
-    header: "Label",
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "billboard",
+    header: "Billboard",
+    cell: ({row}) => row.original.billboardLabel,
   },
   {
     accessorKey: "createdAt",

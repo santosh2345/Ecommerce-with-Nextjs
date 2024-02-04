8fa9414818e973e1a5c8a5a50a9e1930cd95449f
santosh2345 <santoshgiri2345@gmail.com>
2024-02-04 20:36:35 +0545

    category create form

--- admin/app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/components/category-form.tsx
+++ admin/app/(dashboard)/[storeId]/(routes)/categories/[categoryId]/components/category-form.tsx
@@ -97,7 +97,7 @@ const CategoryForm: React.FC<CategoryFormProps> = ({
       toast.success("Category deleted");
     } catch (error) {
       toast.error(
-        "Make sure you removed all categories using this billboard first."
+        "Make sure you removed all products using this category first."
       );
     } finally {
       setLoading(false);

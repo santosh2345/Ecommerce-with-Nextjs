c4505ba16f50c8a524ed9dd4d78a2673765192b5
santosh2345 <santoshgiri2345@gmail.com>
2024-02-04 21:14:15 +0545

    category delete route finished

--- admin/app/api/[storeId]/categories/[categoryId]/route.ts
+++ admin/app/api/[storeId]/categories/[categoryId]/route.ts
@@ -2,6 +2,7 @@ import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
+// Path: admin/app/api/%5BstoreId%5D/categories/%5BcategoryId%5D/route.ts
 export async function GET(
   req: Request,
   { params }: { params: { categoryId: string } }
@@ -23,6 +24,7 @@ export async function GET(
   }
 }
 
+// Path: admin/app/api/%5BstoreId%5D/categories/route.ts
 export async function PATCH(
   req: Request,
   { params }: { params: { storeId: string; categoryId: string } }
@@ -49,6 +51,7 @@ export async function PATCH(
       return new NextResponse("Category id is required", { status: 400 });
     }
 
+      // CHECK IF THE STORE EXISTS
     const storeByUserId = await prismadb.store.findFirst({
       where: {
         id: params.storeId,
@@ -60,6 +63,7 @@ export async function PATCH(
       return new NextResponse("Unauthorized", { status: 403 });
     }
 
+    // UPDATE THE CATEGORY
     const category = await prismadb.category.updateMany({
       where: {
         id: params.categoryId,
@@ -77,6 +81,7 @@ export async function PATCH(
   }
 }
 
+// Path: admin/app/api/%5BstoreId%5D/categories/route.ts
 export async function DELETE(
   req: Request,
   { params }: { params: { storeId: string; categoryId: string } }
@@ -92,6 +97,7 @@ export async function DELETE(
       return new NextResponse("Category ID is required", { status: 400 });
     }
 
+    // CHECK IF THE STORE EXISTS
     const storeByUserId = await prismadb.store.findFirst({
       where: {
         id: params.storeId,
@@ -103,6 +109,7 @@ export async function DELETE(
       return new NextResponse("Unauthorized", { status: 403 });
     }
 
+    // DELETE THE CATEGORY
     const category = await prismadb.category.deleteMany({
       where: {
         id: params.categoryId,
--- admin/app/api/[storeId]/categories/route.ts
+++ admin/app/api/[storeId]/categories/route.ts
@@ -2,6 +2,8 @@ import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
+
+// Path: admin/app/api/%5BstoreId%5D/categories/route.ts
 export async function POST(
   req: Request,
   { params }: { params: { storeId: string } }
@@ -11,20 +13,27 @@ export async function POST(
     const body = await req.json();
     const { name, billboardId } = body;
 
+    // CHECK IF THE USER IS AUTHENTICATED
+
     if (!userId) {
       return new NextResponse("Unauthenticated", { status: 401 });
     }
+    // CHECK IF THE NAME AND BILLBOARD ID EXISTS
     if (!name) {
       return new NextResponse("Name is required", { status: 400 });
     }
+    // CHECK IF THE BILLBOARD ID EXISTS
     if (!billboardId) {
       return new NextResponse("Billboard Id is required", { status: 400 });
     }
+    // CHECK IF THE STORE ID EXISTS
 
     if (!params.storeId) {
       return new NextResponse("Store id is required", { status: 400 });
     }
 
+
+    // CHECK IF THE STORE EXISTS
     const storeByUserId = await prismadb.store.findFirst({
       where: {
         id: params.storeId,
@@ -36,6 +45,7 @@ export async function POST(
       return new NextResponse("Unauthorized", { status: 403 });
     }
 
+    // CREATE A CATEGORY
     const category = await prismadb.category.create({
       data: {
         name,
@@ -44,6 +54,7 @@ export async function POST(
       },
     });
 
+    // RETURN THE CATEGORY
     return NextResponse.json(category);
   } catch (error) {
     console.log("[CATEGORY_POST]", error);
@@ -51,6 +62,8 @@ export async function POST(
   }
 }
 
+// Path: admin/app/api/%5BstoreId%5D/categories/route.ts
+//  GET /api/:storeId/categories
 export async function GET(
   req: Request,
   { params }: { params: { storeId: string } }
@@ -60,12 +73,14 @@ export async function GET(
       return new NextResponse("Store id is required", { status: 400 });
     }
 
+    // CREATE A QUERY TO GET ALL CATEGORIES BY STORE ID
     const category = await prismadb.category.findMany({
       where: {
         storeId: params.storeId,
       },
     });
 
+    // RETURN THE CATEGORIES
     return NextResponse.json(category);
   } catch (error) {
     console.log("[CATEGORY_POST", error);

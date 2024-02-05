84388f66e3a6ce70e2c687de552d7b91f07d138e
santosh2345 <santoshgiri2345@gmail.com>
2024-02-05 17:22:34 +0545

    size route

---
+++ admin/app/(dashboard)/[storeId]/(routes)/sizes/page.tsx
@@ -0,0 +1,33 @@
+import {format} from "date-fns";
+import { BillboardClient } from "./components/client";
+import prismadb from "@/lib/prismadb";
+import { BillboardColumn } from "./components/columns";
+
+const Billboards = async ({ params }: { params: { storeId: string } }) => {
+  const billboards = await prismadb.billboard.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  const formattedBillboards: BillboardColumn[] = billboards.map(
+    (billboard) => ({
+      id: billboard.id,
+      label: billboard.label,
+      createdAt: format(billboard.createdAt, "MMMM do, yyyy"),
+    })
+  );
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <BillboardClient data={formattedBillboards} />
+      </div>
+    </div>
+  );
+};
+
+export default Billboards;

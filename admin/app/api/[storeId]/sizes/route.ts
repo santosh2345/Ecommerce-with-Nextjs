b921a4f3b92197fc548a315092ea2a59caffb188
santosh2345 <santoshgiri2345@gmail.com>
2024-02-05 18:13:15 +0545

    size all routes are done

--- admin/app/api/[storeId]/sizes/route.ts
+++ admin/app/api/[storeId]/sizes/route.ts
@@ -2,6 +2,7 @@ import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
+// Path: admin/app/api/%5BstoreId%5D/sizes/route.ts
 export async function POST(
   req: Request,
   { params }: { params: { storeId: string } }

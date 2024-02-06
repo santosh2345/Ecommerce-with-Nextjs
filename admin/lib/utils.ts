6be7a59d17486dcc671b32dd83a9d0efb48eb5f8
santosh2345 <santoshgiri2345@gmail.com>
2024-02-06 18:46:37 +0545

    formatter

--- admin/lib/utils.ts
+++ admin/lib/utils.ts
@@ -1,6 +1,12 @@
 import { type ClassValue, clsx } from "clsx"
+import { format } from "path"
 import { twMerge } from "tailwind-merge"
  
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const formatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+})

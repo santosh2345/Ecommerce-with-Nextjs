7d65284964eace3d3995b75630ddc54d91875ace
santosh2345 <santoshgiri2345@gmail.com>
2024-02-07 20:25:13 +0545

    shadcn ui checkboc

--- admin/app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
+++ admin/app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
@@ -327,7 +327,10 @@ const ProductForm: React.FC<ProductFormProps> = ({
               render={({ field }) => (
                 <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4">
                <FormControl>
-                <Checkbox />
+                <Checkbox 
+                checked={field.value}
+                onCheckedChange={field.onChange}
+                />
 
                 </FormControl>  
              

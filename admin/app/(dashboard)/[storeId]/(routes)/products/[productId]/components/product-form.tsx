0ac195cb3fc7c953b0e24aea8380568b762f127c
santosh2345 <santoshgiri2345@gmail.com>
2024-02-09 20:58:43 +0545

    product form

--- admin/app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
+++ admin/app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
@@ -126,7 +126,7 @@ const ProductForm: React.FC<ProductFormProps> = ({
       toast.success("Product deleted");
     } catch (error) {
       toast.error(
-        "Something went 11"
+        "Something went 12"
       );
     } finally {
       setLoading(false);

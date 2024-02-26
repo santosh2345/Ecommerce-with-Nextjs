e8226bbcf53a98c5c1339e5e748f672600316a88
santosh2345 <santoshgiri2345@gmail.com>
2024-02-26 14:38:00 +0545

    changes

--- admin/app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
+++ admin/app/(dashboard)/[storeId]/(routes)/products/[productId]/components/product-form.tsx
@@ -362,7 +362,7 @@ const ProductForm: React.FC<ProductFormProps> = ({
                 <div className="space-y-1 leading-none">
                 <FormLabel>Archived</FormLabel>
                   <FormDescription>
-                    This product will not appear anywhere in the 
+                    This product will not appear anywhere in the s
                   </FormDescription>
 
                   </div> 
